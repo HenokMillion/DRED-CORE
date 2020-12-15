59378259920bc7712f212f74e9ab241dfc580e9a
Henok Million <henokmillionmekuria@gmail.com>
2020-12-15 22:29:58 +0300

    mongoose timeout"

--- index.js
+++ index.js
@@ -46,6 +46,7 @@ const dbOptions = {
   connectTimeoutMS: 9000,
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  connectTimeoutMS: 10000
 };
 const connectDb = () => {
   mongoose.connect(process.env.MONGODB_URL, dbOptions);
